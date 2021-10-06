84737b46f4c5027a0e85bbde8558328bef574d30
Robert Curry <robert.curry@lexer.io>
2021-10-07 09:24:48 +1100

    Updates

---
+++ precache-manifest.60a7a25b841cc9a5fa4a55a456731eb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d410cd1cf6438ffed3eca79dc7e04c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "./manifest.json"
+  },
+  {
+    "revision": "8275936b473d40a39544",
+    "url": "./static/css/main.f77a684f.chunk.css"
+  },
+  {
+    "revision": "8275936b473d40a39544",
+    "url": "./static/js/main.8275936b.chunk.js"
+  },
+  {
+    "revision": "d5752d2fdce3c7e150f5",
+    "url": "./static/js/runtime~main.d5752d2f.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "./static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
